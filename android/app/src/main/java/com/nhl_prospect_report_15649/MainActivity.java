157230ed159bf3b17f4d778f04f2bca1e19121af
Crowdbotics <team@crowdbotics.com>
2020-04-09 17:22:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nhl_prospect_report_15649/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nhl_prospect_report_15649;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "nhl_prospect_report_15649";
+    }
+}
